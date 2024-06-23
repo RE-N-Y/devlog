2a431ed9ce44f675b8f497a041204243dcc865c8
Sangwu Lee <sangwulee2@gmail.com>
2024-06-23 18:28:02 +0900

    graduation overhaul + astro v4 upgrade

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,21 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, squooshImageService } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import rehypeKatex from "rehype-katex"
 import remarkMath from 'remark-math';
 
-import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://nyre.github.io',
   base: '/devlog',
-  experimental: { assets: true },
+  image: {
+    service: squooshImageService(),
+  },
   integrations: [
-    mdx({ remarkPlugins: [remarkMath], rehypePlugins:[rehypeKatex] }), 
+    mdx({ rehypePlugins: [rehypeKatex], remarkPlugins: [remarkMath]}),
     sitemap(), 
     tailwind({config: { applyBaseStyles: false }}), 
-    image()
   ]
 });
