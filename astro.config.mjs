6eb98c7d7d8b239545d584a91cccc3c12ad765a8
Sangwu Lee <sangwulee2@gmail.com>
2024-06-27 16:22:16 +0900

    fix site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,20 +2,25 @@ import { defineConfig, squooshImageService } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
-import rehypeKatex from "rehype-katex"
+import rehypeKatex from "rehype-katex";
 import remarkMath from 'remark-math';
+import partytown from "@astrojs/partytown";
 
+import playformCompress from "@playform/compress";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://nyre.github.io',
+  site: 'https://re-n-y.github.io/',
   base: '/devlog',
   image: {
-    service: squooshImageService(),
+    service: squooshImageService()
   },
-  integrations: [
-    mdx({ rehypePlugins: [rehypeKatex], remarkPlugins: [remarkMath]}),
-    sitemap(), 
-    tailwind({config: { applyBaseStyles: false }}), 
-  ]
+  integrations: [mdx({
+    rehypePlugins: [rehypeKatex],
+    remarkPlugins: [remarkMath]
+  }), sitemap(), tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }), partytown(), playformCompress({ CSS: true, Image:true })]
 });
